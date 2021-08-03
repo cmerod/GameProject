af97e37fe635b3d6a121834c7e71498972001efb
Marcus Rodriguez <rodri@kth.se>
2021-08-03 16:19:06 +0200

    GamePanel implements Runnable and KeyListener.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,12 +1,51 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable, KeyListener {
     private static final int GAME_WIDTH = 1920, GAME_HEIGHT = 1080;
     private static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
+
+    private Thread gameThread;
 
     public GamePanel(){
         this.setFocusable(true);
         this.setPreferredSize(SCREEN_SIZE);
+        this.addKeyListener(this);
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime();
+        double ns = 1000000000/60.0;
+        double delta = 0;
+        while(true){
+            long now = System.nanoTime();
+            delta += (now-lastTime)/ns;
+            lastTime = now;
+            if(delta >= 1){
+
+                delta--;
+            }
+        }
     }
 }
